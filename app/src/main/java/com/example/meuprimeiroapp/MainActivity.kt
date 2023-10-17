c92706e528e66f239a75b71f8f22ea1bffab59a8
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2023-10-17 19:51:28 -0300

    App de Sorteio pronto

--- app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.meuprimeiroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+        // aprindo classe view
+        fun sortear(view: View){
+         val resultado = findViewById<TextView>(R.id.text_Resultado)//find encontrar
+         val numero= Random().nextInt(100)//random sortear
+            resultado.setText("Numero sorteado é: $numero") //
+
+        }
+
 }
